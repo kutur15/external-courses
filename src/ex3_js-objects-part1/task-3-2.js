78819c47b34af8d65ce96d4b7124f92bcf3c7a4a
kutur15 <kutur15@hotmail.com>
2018-10-14 16:32:29 +0300

    task3 part2

--- src/ex3_js-objects-part1/task-3-2.js
+++ src/ex3_js-objects-part1/task-3-2.js
@@ -2,9 +2,10 @@ var obj = {};
 obj.a = 1;
 obj.b = "phone";
 obj.c = null;
+var key
 function MyFunc1 (obj){
 
-    for (var key in obj) {
+    for (key in obj) {
         console.log("Ключ: " + key + " значение: " + obj[key]);
     }
 
