3732cea8bb45096c55b0615b50879404fc11e00c
kutur15 <kutur15@hotmail.com>
2018-10-15 08:53:21 +0300

    task03

--- src/ex3_js-objects-part1/task-3-4.js
+++ src/ex3_js-objects-part1/task-3-4.js
@@ -1,13 +1,8 @@
-var obj = {};
-var key;
-var n = "new";
-function MyFunc1 (n,obj){
-for (key in obj) {
-    if (!obj.hasOwnProperty(key))
-    { obj.key = "new" };
 
+function MyFunc1 (n,obj){
+
+if(n in obj) return obj;
+obj[n] = 'new';
+return obj;
 };
-         
-    return (obj[key])
-      };
-module.exports = MyFunc1 ();
+module.exports = MyFunc1;
