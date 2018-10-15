92cfe1e9dfaf664e697c1e84d205eb6751785e47
kutur15 <kutur15@hotmail.com>
2018-10-15 10:35:14 +0300

    remove 3 HW tasks

--- src/ex3_js-objects-part1/task-3-1.js
+++
@@ -1,5 +0,0 @@
-var obj = {};
-    obj.a = 1;
-    obj.b = "phone";
-    obj.c = null;
-    delete obj.b;
--- src/ex3_js-objects-part1/task-3-4.js
+++
@@ -1,8 +0,0 @@
-
-function MyFunc1 (n,obj){
-var result = obj;
-if(n in result) {return result;}
-result[n] = 'new';
-return result;
-}
-module.exports = MyFunc1;
